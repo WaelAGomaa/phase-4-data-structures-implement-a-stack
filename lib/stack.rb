94177a27996447b0d0f3d86eef74c03944e6ecfa
wael <waeelash155@gmail.com>
2022-06-07 21:37:54 -0400

    commit tree ca433fce0adbce33932b3581b36d427d78a4628d

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,16 @@
-# your code here
+class Stack
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+    def push(element)
+        # raise 'Stack Overflow' if full?
+        @stack.push(element)
+    end
+    def pop
+        @stack.pop()
+    end
+    def peek
+        @stack.last()
+    end
+end
